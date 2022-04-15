1c44a6cb2023a75d80e4d46b2fc34a62fc8ef177
Job85 <julianjernigan@ymail.com>
2022-04-15 14:15:52 -0400

    added and migrated comments

---
+++ migrations/20220415181005-create-comments.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('comments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      body: {
+        type: Sequelize.STRING
+      },
+      likes: {
+        type: Sequelize.STRING
+      },
+      post_id: {
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('comments');
+  }
+};
---
+++ models/comments.js
@@ -0,0 +1,51 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Comments extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Comments.init({
+    body: {
+      type:DataTypes.STRING,
+      allowNull: false
+    }
+    likes: {
+      type:DataTypes.STRING,
+    allowNull: false
+  }
+    postId:{
+      type:DataTypes.INTEGER,
+      allowNull: false,
+      field: 'post_id',
+      onDelete: 'CASCADE',
+      references: {
+        model: 'posts',
+        key: 'id'
+      }
+    }
+    userId: {
+      type:DataTypes.INTEGER,
+      allowNull: false,
+      field: 'post_id',
+      onDelete: 'CASCADE',
+      references: {
+        model: 'users',
+        key: 'id'
+      }
+  }, 
+  {
+    sequelize,
+    modelName: 'Comments',
+    tableName: 'comments'
+  });
+  return Comments;
+};
