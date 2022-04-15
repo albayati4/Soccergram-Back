9010d3b3db262e4eb212bc0a4b58829f3ca3ad8a
Job85 <julianjernigan@ymail.com>
2022-04-15 16:00:45 -0400

    added PostRouter.js and logic

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -1,4 +1,6 @@
 const Router = require('express').Router()
 const AuthRouter = require('express').Router()
+const UserRouter = require('express').Router()
+
 
 module.exports = Router
---
+++ routes/PostRouter.js
@@ -0,0 +1,26 @@
+const Router = require('express').Router()
+const controller = require('../controllers/PostController')
+const middleware = require('../middleware')
+
+Router.get('/', controller.GetPosts)
+Router.post(
+    '/'
+    middleware.stripToken,
+    middleware.verifyToken,
+    controller.CreatePost
+)
+Router.put(
+    '/:post_id',
+    middleware.stripToken,
+    middleware.verifyToken,
+    controller.UpdatePost
+)
+
+Router.delete(
+    '/:post_id',
+    middleware.stripToken,
+    middleware.verifyToken,
+    controller.DeletePost
+)
+
+module.exports = Router
