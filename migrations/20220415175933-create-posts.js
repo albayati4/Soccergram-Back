2581b4f278f2403f9fbd29a8b3bd92979c2eb180
Job85 <julianjernigan@ymail.com>
2022-04-15 14:06:19 -0400

    added posts model

---
+++ migrations/20220415175933-create-posts.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('posts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      body: {
+        type: Sequelize.STRING
+      },
+      user_id: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('posts');
+  }
+};
---
+++ models/posts.js
@@ -0,0 +1,40 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Posts extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Posts.init({
+    title: {
+      type:DataTypes.STRING,
+      allowNull: false
+    }
+    body: {
+      type:DataTypes.STRING,
+      allowNull: false}
+    userId: {
+      type:DataTypes.INTEGER
+      allowNull: false,
+      field: 'user_id',
+      onDelete: 'CASCADE',
+      references: {
+        model: 'users',
+        key: 'id'
+      } 
+    }
+  }, {
+    sequelize,
+    modelName: 'Posts',
+    tableName: 'posts'
+  });
+  return Posts;
+};
