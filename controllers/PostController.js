ce063698fb403f56febf38156b1bd5e214e93dbb
Job85 <julianjernigan@ymail.com>
2022-04-18 16:59:05 -0400

    DeletePost function working

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -37,29 +37,29 @@ const UpdatePost = async (req, res) => {
 }
 
 // delete post by id
-const DeletePost = async (req, res) => {
-    try {
-        const postId = req.params.id
-        const deletedPost = await postModel.findByIdAndDelete(postId)
-        res.send(deletedPost, { message: `Post ${postId} was deleted` })
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({
-            error: error.message
-        })
-    }
-}
-
 // const DeletePost = async (req, res) => {
 //     try {
-
-//         let postId = parseInt(req.params.postId)
-//         await Post.destroy({ where: { id: postId } })
-//         res.send({ message: `Post ${postId} was deleted` })
+//         const postId = req.params.id
+//         const deletedPost = await postModel.findByIdAndDelete(postId)
+//         res.send(deletedPost, { message: `Post ${postId} was deleted` })
 //     } catch (error) {
-//         throw error
+//         console.log(error)
+//         res.status(500).json({
+//             error: error.message
+//         })
 //     }
 // }
+
+const DeletePost = async (req, res) => {
+    try {
+
+        let postId = parseInt(req.params.post_id)
+        await Posts.destroy({ where: { id: postId } })
+        res.send({ message: `Post ${postId} was deleted` })
+    } catch (error) {
+        throw error
+    }
+}
 
 
 module.exports = {
