249bf64b4f70cddeaace7ebc6e65f42903499241
Job85 <julianjernigan@ymail.com>
2022-04-19 12:11:03 -0400

    added GetUserFollowing route

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -2,6 +2,7 @@ const Router = require('express').Router()
 const controller = require('../controllers/UserController')
 
 Router.get('/', controller.GetUsers)
+Router.get('/:user_id/following', controller.GetUserFollowing)
 
 
 module.exports = Router
