c471c193f0a2a7a4eb62b20b2b8a2d9a59865d42
Job85 <julianjernigan@ymail.com>
2022-04-20 09:11:34 -0400

    deleted muted code, changed deleted post message

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -10,7 +10,6 @@ const GetPosts = async (req, res) => {
     }
 }
 
-
 const CreatePost = async (req, res) => {
     try {
         let buildBody = {
@@ -36,26 +35,12 @@ const UpdatePost = async (req, res) => {
     }
 }
 
-// delete post by id
-// const DeletePost = async (req, res) => {
-//     try {
-//         const postId = req.params.id
-//         const deletedPost = await postModel.findByIdAndDelete(postId)
-//         res.send(deletedPost, { message: `Post ${postId} was deleted` })
-//     } catch (error) {
-//         console.log(error)
-//         res.status(500).json({
-//             error: error.message
-//         })
-//     }
-// }
-
 const DeletePost = async (req, res) => {
     try {
 
         let postId = parseInt(req.params.post_id)
         await Posts.destroy({ where: { id: postId } })
-        res.send({ message: `Post ${postId} was deleted` })
+        res.send({ message: `Your post, ${postId}, was deleted` })
     } catch (error) {
         throw error
     }
