6c38b87e6b66a4dfe4c9e768df634445295cf1b8
Job85 <julianjernigan@ymail.com>
2022-04-20 10:24:05 -0400

    added more post to db from seeder

--- seeders/20220419143402-demo-post.js
+++ seeders/20220419143402-demo-post.js
@@ -5,15 +5,15 @@ const { query } = require("express");
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert('posts', [{
-      title: 'My Very First Post',
-      body: 'Today I created my first post on Soccergram!',
+      title: 'First Game of the Season',
+      body: 'Our first game was today! Sadly, we have a lot to work on',
       user_id: 1,
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
-      title: 'I love soccer',
-      body: 'I love soccer so much. That\'s why I joined Soccergram',
-      user_id: 2,
+      title: 'Improving',
+      body: 'We\'re getting better but still more work needs to be done',
+      user_id: 1,
       createdAt: new Date(),
       updatedAt: new Date()
     }])
