b8a348d17a5fc5b25b7d5b0c3d1f4d13e61077e3
Job85 <julianjernigan@ymail.com>
2022-04-20 13:44:11 -0400

    added GetProfile route

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -3,6 +3,7 @@ const controller = require('../controllers/UserController')
 
 
 Router.get('/', controller.GetUsers)
+Router.get('/profile/:user_id', controller.GetProfile)
 Router.get('/:user_id/following', controller.GetUserFollowing)
 Router.get('/:user_id/followers', controller.GetUserFollowers)
 
