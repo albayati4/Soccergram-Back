84dcef87b718e1dfbf256a855830fd7578f494ef
Job85 <julianjernigan@ymail.com>
2022-04-21 13:38:32 -0400

    added demo seeders

--- seeders/20220419143402-demo-post.js
+++ seeders/20220419143402-demo-post.js
@@ -5,9 +5,27 @@ const { query } = require("express");
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert('posts', [{
-      title: 'My Second Post',
-      body: 'This is my second post post of the season!',
+      title: 'My Demo Post',
+      body: 'This is my demo post!',
+      user_id: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      title: 'My Test Post',
+      body: 'This is my test post!',
       user_id: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      title: 'My Example Post',
+      body: 'This is my example post!',
+      user_id: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      title: 'My Trial Post',
+      body: 'This is my trial post!',
+      user_id: 4,
       createdAt: new Date(),
       updatedAt: new Date()
     }])
--- seeders/20220421041547-demo-comments.js
+++ seeders/20220421041547-demo-comments.js
@@ -1,32 +1,32 @@
-'use strict';
+// 'use strict';
 
-module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('comments', [{
-      body: 'Welcome to SoccerGram!',
-      likes: '',
-      post_id: 2,
-      user_id: 2,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }, {
-      body: 'I love Soccer too! Who\'s you\'re team?',
-      likes: '',
-      post_id: 2,
-      user_id: 1,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }, {
-      body: 'You play soccer? Or just a fan?',
-      likes: '',
-      post_id: 4,
-      user_id: 3,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }])
-  },
+// module.exports = {
+//   async up(queryInterface, Sequelize) {
+//     await queryInterface.bulkInsert('comments', [{
+//       body: 'Welcome to SoccerGram!',
+//       likes: '',
+//       post_id: 2,
+//       user_id: 2,
+//       createdAt: new Date(),
+//       updatedAt: new Date()
+//     }, {
+//       body: 'I love Soccer too! Who\'s you\'re team?',
+//       likes: '',
+//       post_id: 2,
+//       user_id: 1,
+//       createdAt: new Date(),
+//       updatedAt: new Date()
+//     }, {
+//       body: 'You play soccer? Or just a fan?',
+//       likes: '',
+//       post_id: 4,
+//       user_id: 3,
+//       createdAt: new Date(),
+//       updatedAt: new Date()
+//     }])
+//   },
 
-  async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('comments', null, {})
-  }
-};
+//   async down(queryInterface, Sequelize) {
+//     await queryInterface.bulkDelete('comments', null, {})
+//   }
+// };
