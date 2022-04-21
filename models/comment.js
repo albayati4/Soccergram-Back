70dbbc2121478fc171482e974ff67a20054dcee5
Job85 <julianjernigan@ymail.com>
2022-04-21 00:53:07 -0400

    defined association to Post model

--- models/comment.js
+++ models/comment.js
@@ -11,6 +11,18 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Comment.belongsTo(models.User, {
+        foreignKey: 'user_id',
+        as: 'user',
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+      })
+      Comment.belongsTo(models.Posts, {
+        foreignKey: 'post_id',
+        as: 'post',
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+      })
     }
   }
   Comment.init({
