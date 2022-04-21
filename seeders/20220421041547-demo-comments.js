ea1d9434b93340434feb8905bb7861fb1684512b
Job85 <julianjernigan@ymail.com>
2022-04-21 13:49:57 -0400

    added demo comments

--- seeders/20220421041547-demo-comments.js
+++ seeders/20220421041547-demo-comments.js
@@ -1,32 +1,39 @@
-// 'use strict';
+'use strict';
 
-// module.exports = {
-//   async up(queryInterface, Sequelize) {
-//     await queryInterface.bulkInsert('comments', [{
-//       body: 'Welcome to SoccerGram!',
-//       likes: '',
-//       post_id: 2,
-//       user_id: 2,
-//       createdAt: new Date(),
-//       updatedAt: new Date()
-//     }, {
-//       body: 'I love Soccer too! Who\'s you\'re team?',
-//       likes: '',
-//       post_id: 2,
-//       user_id: 1,
-//       createdAt: new Date(),
-//       updatedAt: new Date()
-//     }, {
-//       body: 'You play soccer? Or just a fan?',
-//       likes: '',
-//       post_id: 4,
-//       user_id: 3,
-//       createdAt: new Date(),
-//       updatedAt: new Date()
-//     }])
-//   },
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('comments', [{
+      body: 'This is a demo comment! Hey from user_id:2.',
+      likes: '',
+      post_id: 1,
+      user_id: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      body: 'This is a test comment. Howdy from user_id:1!',
+      likes: '',
+      post_id: 2,
+      user_id: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      body: 'This is an example comment. User_id:3, here',
+      likes: '',
+      post_id: 3,
+      user_id: 4,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      body: 'This is an trial comment. User_id:4, here',
+      likes: '',
+      post_id: 4,
+      user_id: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }])
+  },
 
-//   async down(queryInterface, Sequelize) {
-//     await queryInterface.bulkDelete('comments', null, {})
-//   }
-// };
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('comments', null, {})
+  }
+};
