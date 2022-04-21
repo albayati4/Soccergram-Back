7cef86097e429ae754fb518a4820985ade5299a1
Job85 <julianjernigan@ymail.com>
2022-04-21 00:32:20 -0400

    created logic for bulk insert

--- seeders/20220421041547-demo-comments.js
+++ seeders/20220421041547-demo-comments.js
@@ -1,24 +1,26 @@
 'use strict';
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('comments', {
+      body: 'Welcome to SoccerGram!',
+      likes: '',
+      postId: 2,
+      userId: 2,
+    }, {
+      body: 'I love Soccer too! Who\'s you\'re team?',
+      likes: '',
+      postId: 2,
+      userId: 1,
+    }, {
+      body: 'You play soccer? Or just a fan?',
+      likes: '',
+      postId: 4,
+      userId: 3,
+    })
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('comments', null, {})
   }
 };
