184e75d09c926ee96342c629f36df86fbfc4f307
Job85 <julianjernigan@ymail.com>
2022-04-21 00:59:55 -0400

    added CommentRouter

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -1,11 +1,13 @@
 const Router = require('express').Router()
 const AuthRouter = require('./AuthRouter')
-const UserRouter = require('./UserRouter')
+const CommentRouter = require('/CommentRouter')
 const PostRouter = require('./PostRouter')
+const UserRouter = require('./UserRouter')
 
 Router.use('/auth', AuthRouter)
-Router.use('/user', UserRouter)
+Router.use('/comment', CommentRouter)
 Router.use('/post', PostRouter)
+Router.use('/user', UserRouter)
 
 
 module.exports = Router
