5e4122fc3e40eb42ab2a8b99d6f8b115d32f0661
Job85 <julianjernigan@ymail.com>
2022-04-21 09:21:11 -0400

    added DeletePostByUserId

--- routes/PostRouter.js
+++ routes/PostRouter.js
@@ -6,5 +6,5 @@ Router.get('/user_feed/:user_id', controller.GetUserPosts)
 Router.post('/', controller.CreatePost)
 Router.put('/:post_id', controller.UpdatePost)
 Router.delete('/:post_id', controller.DeletePost)
-
+Router.delete('/:post_id/:user_id', controller.DeletePostByUserId)
 module.exports = Router
