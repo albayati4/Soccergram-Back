e9e8a6a5b07fa3c2444efdd9b988d1165c480af2
Job85 <julianjernigan@ymail.com>
2022-04-21 01:37:47 -0400

    added route to GetPostComments

--- routes/CommentRouter.js
+++ routes/CommentRouter.js
@@ -2,5 +2,6 @@ const Router = require('express').Router()
 const controller = require("../controllers/CommentController")
 
 Router.get('/', controller.GetComments)
+Router.get('/:post_id', controller.GetPostComments)
 
 module.exports = Router
