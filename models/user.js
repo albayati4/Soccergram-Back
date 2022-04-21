0a8d3d6fd1f1f81191794fda28045d2a7d6f3500
Job85 <julianjernigan@ymail.com>
2022-04-21 00:48:12 -0400

    defined association to Comment model

--- models/user.js
+++ models/user.js
@@ -11,6 +11,12 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      User.hasMany(models.Comment, {
+        foreignKey: 'user_id',
+        as: 'user',
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+      })
       User.hasMany(models.Posts, {
         foreignKey: 'user_id',
         as: 'user',
