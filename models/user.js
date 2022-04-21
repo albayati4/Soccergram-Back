9a8689987315b6333a1ff7195bb453937576b437
Job85 <julianjernigan@ymail.com>
2022-04-21 01:21:42 -0400

    changed alias in User Comment to user_comment

--- models/user.js
+++ models/user.js
@@ -11,12 +11,12 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      // User.hasMany(models.Comment, {
-      //   foreignKey: 'user_id',
-      //   as: 'user',
-      //   onDelete: 'CASCADE',
-      //   onUpdate: 'CASCADE'
-      // })
+      User.hasMany(models.Comment, {
+        foreignKey: 'user_id',
+        as: 'user_comment',
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+      })
       User.hasMany(models.Posts, {
         foreignKey: 'user_id',
         as: 'user',
