082bc7dc5f570ddb81efc6d7b5a45fbef76841d1
Job85 <julianjernigan@ymail.com>
2022-04-21 09:50:39 -0400

    updated put route

--- routes/PostRouter.js
+++ routes/PostRouter.js
@@ -4,7 +4,7 @@ const controller = require('../controllers/PostController')
 Router.get('/', controller.GetPosts)
 Router.get('/user_feed/:user_id', controller.GetUserPosts)
 Router.post('/', controller.CreatePost)
-Router.put('/:post_id', controller.UpdatePost)
+Router.put('/:post_id/:user_id', controller.UpdatePost)
 Router.delete('/:post_id', controller.DeletePost)
 Router.delete('/:post_id/:user_id', controller.DeletePostByUserId)
 
