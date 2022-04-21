b38ab5f053873e92f6e5372b5794ffd2f4522e39
Job85 <julianjernigan@ymail.com>
2022-04-21 17:21:46 -0400

    updated put and post route

--- routes/PostRouter.js
+++ routes/PostRouter.js
@@ -2,9 +2,9 @@ const Router = require('express').Router()
 const controller = require('../controllers/PostController')
 
 Router.get('/', controller.GetPosts)
-Router.get('/user_feed/:user_id', controller.GetUserPosts)
-Router.post('/', controller.CreatePost)
-Router.put('/:post_id/:user_id', controller.UpdatePost)
+Router.get('/:user_id', controller.GetUserPosts)
+Router.post('/:user_id', controller.CreatePost)
+Router.put('/:post_id', controller.UpdatePost)
 Router.delete('/:post_id', controller.DeletePost)
 Router.delete('/:post_id/:user_id', controller.DeletePostByUserId)
 
