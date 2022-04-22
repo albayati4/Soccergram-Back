16195893a87d48eaf0f41c1e239270db185d804c
Job85 <julianjernigan@ymail.com>
2022-04-22 11:49:37 -0400

    deleted blank spaces

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 
 const app = express()
 
+const PORT = process.env.PORT || 3001
 
 
 app.use(cors())
@@ -10,7 +11,6 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 const AppRouter = require('./routes/AppRouter')
 
-const PORT = process.env.PORT || 3001
 
 app.get('/', (req, res) => res.json({ message: 'Server Works' }))
 app.use('/api', AppRouter)
